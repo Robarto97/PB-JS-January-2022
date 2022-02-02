952b2ae8fb81354be1d4322f0be677fcca5e1c45
Robarto <robartpetrov@gmail.com>
2022-02-02 14:52:28 +0200

    final

--- For Loops/Clock.js
+++ For Loops/Clock.js
@@ -1,17 +1,7 @@
 function clock() {
-  let minutes = 0;
-  let hours = 0;
-  console.log(`${hours} : ${minutes}`);
   for (let i = 0; i < 24; i++) {
     for (let j = 0; j < 60; j++) {
-      minutes++;
-      if (minutes == 60) {
-        hours++;
-        minutes = 0;
-      }
-      if (hours <= 23) {
-        console.log(`${hours} : ${minutes}`);
-      }
+      console.log(`${i} : ${j}`);
     }
   }
 }
--- For Loops/ClockPart2.js
+++ For Loops/ClockPart2.js
@@ -1,23 +1,8 @@
 function clock() {
-  let minutes = 0;
-  let hours = 0;
-  let seconds = 0;
-  console.log(`${hours} : ${minutes} : ${seconds}`);
   for (let i = 0; i < 24; i++) {
     for (let j = 0; j < 60; j++) {
       for (let k = 0; k < 60; k++) {
-        seconds++;
-        if (seconds == 60) {
-          minutes++;
-          seconds = 0;
-        }
-        if (minutes == 60) {
-          minutes = 0;
-          hours++;
-        }
-        if (hours <= 23) {
-          console.log(`${hours} : ${minutes} : ${seconds}`);
-        }
+        console.log(`${i} : ${j} : ${k}`);
       }
     }
   }
